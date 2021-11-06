09cd3b3d2f6050047ae4402e579480d5dcf32247
jdbecerra <jdbecerra1202@users.noreply.github.com>
2021-11-06 00:54:19 -0500

    finished menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../styles/Navbar.scss"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -7,31 +8,44 @@ import Logo from "../assets/logo-vass-blanco.png";
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg fixed-top">
+        <nav className="navbar navbar-dark navbar-expand-lg fixed-top">
             <div className="container-fluid">
                 <a href="#" className="navbar-brand">
                     <img src={Logo}/>
                 </a>
 
                 <div className="">
-                    <button type="button" className="navbar-toggler mr-2" data-toggle="collapse" data-target="#menu-principal" aria-controls="menu-principal" aria-expanded="false">
-                        <span className="text-white"><FontAwesomeIcon icon={faBars}/></span>
-                    </button>
-
+                    
                     <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#buscador" aria-controls="buscador" aria-expanded="false">
                         <span className="text-white"><FontAwesomeIcon icon={faSearch}/></span>
+                    </button>
+
+                    <button type="button" className="navbar-toggler ml-2" data-toggle="collapse" data-target="#menu-principal" aria-controls="menu-principal" aria-expanded="false">
+                        <span className="text-white"><FontAwesomeIcon icon={faBars}/></span>
                     </button>
                 </div>
 
                 <div className="collapse navbar-collapse" id="buscador">
-                    <input type="text" className="d-lg-none"/>
+                    <input type="text" className=" divBuscador d-lg-none mt-2"/>
                 </div>
 
                 <div className=" collapse navbar-collapse" id="menu-principal">
-                    <ul className="navbar-nav ml-auto">                        
+                    <ul className="textUl navbar-nav ml-auto">                        
                         <li className="nav-item"><a href="#" className="nav-link">Casos de exito</a></li>
                         <li className="nav-item"><a href="#" className="nav-link">Mercados</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">Paises</a></li>
+                        <li className="nav-item dropdown">
+                            <a className="dropdown-toggle nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Paises</a>
+                            <ul class="dropdown-menu" role="menu">
+                                <div className="divsubmenu">
+                                <li className="nav-item border-left"><a href="#" className="nav-link">Chile</a></li>
+                                <li className="nav-item border-left"><a href="#" className="nav-link">Colombia</a></li>
+                                <li className="nav-item border-left"><a href="#" className="nav-link">Mexico</a></li>
+                                <li className="nav-item border-left"><a href="#" className="nav-link">Peru</a></li>
+                                <li className="nav-item border-left"><a href="#" className="nav-link">Reino Unido</a></li>
+                                <li className="nav-item border-left"><a href="#" className="nav-link">Benelux</a></li>
+                                </div>
+                            </ul>
+                        </li>
                         <li className="nav-item"><a href="#" className="nav-link">Somos VASS</a></li>
                         <li className="nav-item"><a href="#" className="nav-link">Como lo hacemos</a></li>
                         <li className="nav-item"><a href="#" className="nav-link">Insights</a></li>
@@ -42,9 +56,8 @@ export const Navbar = () => {
                         <div className="collapse mt-1" id="buscador2">
                             <input type="text"/>
                         </div>
-                        <li className="nav-item"><a href="#" className="nav-link ml-lg-3 text-primary">Talento</a></li>
+                        <li className="nav-item"><a href="#" className="itemLi nav-link ml-lg-4 text-primary">Talento</a></li>
                     </ul>
-
                     
                 </div>
             </div>
